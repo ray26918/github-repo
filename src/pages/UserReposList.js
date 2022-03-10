27a4f5af1c2152fd6243342749a92b9dd899360b
unknown <a131101533@gmail.com>
2022-03-10 17:51:43 +0800

    infinite scroll complete

--- src/pages/UserReposList.js
+++ src/pages/UserReposList.js
@@ -1,26 +1,83 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { List, message, Avatar, Skeleton, Divider } from 'antd';
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserRepos } from "../redux/action";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function UserReposList() {
 
     const dispatch = useDispatch()
     const userReopsList = useSelector((state) => state.userRepos.userReposList)
-
+    const [ data, setData ] = useState([])
+		const [ hasMoreData, setHasmoreData ] = useState(true)
+		const [ curPage, setCurPage ] = useState(1)
+    const [ loading, setLoading ] = useState(false)
     let { username } = useParams()
 
-    useEffect(() => {
-        dispatch(getUserRepos(username))
-    }, [])
+    const loadMoreData = () => {
+			if (loading) {
+          return;
+        }
+        setLoading(true);
+        dispatch(getUserRepos(username, curPage))
+        .then(resp =>{
+					if( resp.length !== 0 ){
+						setCurPage(curPage + 1)
+						setData([ ...data, ...resp ])
+						setLoading(false)
+					}
+					else{
+						setLoading(false)
+						setHasmoreData(false)
+					}
+				})
+				.catch((resp) => {
+					console.log(resp)
+					setLoading(false)
+				})
+      };
 
     useEffect(() => {
-        console.log(userReopsList)
-    }, [userReopsList])
+	    loadMoreData();
+    }, []);
+      
+    // useEffect(() => {
+		// 	console.log(data)
+    // }, [data])
 
     return(
-        <div>
-            UserReposList pages
-        </div>
+      <div
+        id="scrollableDiv"
+        style={{
+          height: 400,
+          overflow: 'auto',
+          padding: '0 16px',
+          border: '1px solid rgba(140, 140, 140, 0.35)',
+        }}
+      >
+        <InfiniteScroll
+          dataLength={data.length}
+          next={loadMoreData}
+          hasMore={hasMoreData}
+          loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+          endMessage={<Divider plain> 載入完畢 </Divider>}
+          scrollableTarget="scrollableDiv"
+        >
+          <List
+						header={ username }
+            dataSource={data}
+            renderItem={item => (
+              <List.Item key={item.id}>
+                <List.Item.Meta
+                  title={item.name}
+                  description={item.stargazers_count}
+                />
+                <div>Content</div>
+              </List.Item>
+            )}
+          />
+        </InfiniteScroll>
+      </div>
     )
 }
--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,7 +1,11 @@
 import requests from "../api/requests";
 import { GET_USERREPOS } from "./Types";
 
-export const getUserRepos = (userName) => async (dispatch) =>{
-    const resp = await requests.get(`/users/${userName}/repos`)
+export const getUserRepos = (userName, curPage) => async (dispatch) =>{
+    const resp = await requests.get(`/users/${userName}/repos`,{ params: {
+        per_page: 10,
+        page: curPage
+    }})
     dispatch({ type: GET_USERREPOS, payload: resp.data })
+    return resp.data
 }
