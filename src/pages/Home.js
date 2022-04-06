43d456ad36e2293e66b548df0e6c62f4d9dd1e46
Ray <Ray.Wu@wiadvance.com>
2022-04-06 09:17:43 +0800

    validator

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import { Button, Col, Input, Row } from 'antd';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import  validator  from 'validator'
+
 
 export default function Home(){
 
 	const [ inputUserName, setInputUserName ] = useState('')
-
 
 	return(
 		<>
@@ -15,15 +16,17 @@ export default function Home(){
 					</Col>
 					<Col span={4}>
 					<Input
-							style={{ borderRadius: '15px' }}
-							onChange={(e) => setInputUserName(e.target.value)}
+						style={ validator.isEmpty(inputUserName) ? { border: '1px solid red', borderRadius: '15px' } : { borderRadius: '15px' }}
+						onChange={(e) => setInputUserName(e.target.value)}
 					/>
+					{ validator.isEmpty(inputUserName) ? <span style={{ color: 'red' }}>輸入欄位請勿為空!</span> :''}
 					</Col>
-					<Col style={{ paddingLeft: '10px' }} span={4}>
+					<Col style={{ paddingLeft: '10px' }} span={4}>	
 					<Link to={`/home/users/${inputUserName}/repos`}>
 							<Button 
-							type='primary' 
-							shape='round'
+								type='primary' 
+								shape='round'
+								disabled={validator.isEmpty(inputUserName)}
 							>
 							送出
 							</Button>
